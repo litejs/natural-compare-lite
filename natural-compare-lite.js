54bd735886ada10a72c2c59628a614b9fd462d38
Lauri Rooden <lauri@rooden.ee>
2013-04-01 15:51:37 +0300

    Whitespace fixes

--- natural-compare-lite.js
+++ natural-compare-lite.js
@@ -10,27 +10,27 @@
 
 
 String.natural_compare = function(a, b) {
-  var c, ca, cb, ia = 0, ib = 0
+	var c, ca, cb, ia = 0, ib = 0
 
 	if (a !== b) while (ca !== "") {
-    ca = a.charAt( ia++ )
-    cb = b.charAt( ib++ )
+		ca = a.charAt( ia++ )
+		cb = b.charAt( ib++ )
 
-    if (ca > "/" && ca < ":") {
-      for (;c = a.charAt(ia), c > "/" && c < ":";ia++) ca += c
-      ca |= 0
-    }   
+		if (ca > "/" && ca < ":") {
+			for (;c = a.charAt(ia), c > "/" && c < ":";ia++) ca += c
+			ca |= 0
+		}   
 
-    if (cb > "/" && cb < ":") {
+		if (cb > "/" && cb < ":") {
 			// number always comes first
 			if (typeof ca !== "number") return 1
-      for (;c = b.charAt(ib), c > "/" && c < ":";ib++) cb += c
-      cb |= 0
-    } else if (typeof ca === "number") return -1
+			for (;c = b.charAt(ib), c > "/" && c < ":";ib++) cb += c
+			cb |= 0
+		} else if (typeof ca === "number") return -1
 
-    if (ca > cb) return 1
-    if (ca < cb) return -1
-  }
+		if (ca > cb) return 1
+		if (ca < cb) return -1
+	}
 	return 0
 }
 
