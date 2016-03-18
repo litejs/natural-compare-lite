5bb00890621b24a8d9a97a7f3e2dbc61fda64276
Ido Samuelson <ido.samuelson@gmail.com>
2016-03-18 13:08:23 -0400

    improved performance, cleanup

--- index.js
+++ index.js
@@ -9,45 +9,43 @@
  * @license    MIT License
  */
 
+var getCode = function(str, pos, code) {
+	if (code) {
+		for (i = pos; code = getCode(str, i), code < 76 && code > 65;) i++;
+		return +str.slice(pos - 1, i)
+	}
+	
+	code = String.alphabet && String.alphabet.indexOf(str.charAt(pos));
+	return code > -1 ? code + 76 : ((code = str.charCodeAt(pos) || 0), code < 45 || code > 127) ? code
+		: code < 46 ? 65               // -
+		: code < 48 ? code - 1
+		: code < 58 ? code + 18        // 0-9
+		: code < 65 ? code - 11
+		: code < 91 ? code + 11        // A-Z
+		: code < 97 ? code - 37
+		: code < 123 ? code + 5        // a-z
+		: code - 63
+}
 
 var naturalCompare = function(a, b) {
-	var i, codeA
+	var codeA
 	, codeB = 1
 	, posA = 0
 	, posB = 0
-	, alphabet = String.alphabet
 
-	function getCode(str, pos, code) {
-		if (code) {
-			for (i = pos; code = getCode(str, i), code < 76 && code > 65;) ++i;
-			return +str.slice(pos - 1, i)
-		}
-		code = alphabet && alphabet.indexOf(str.charAt(pos))
-		return code > -1 ? code + 76 : ((code = str.charCodeAt(pos) || 0), code < 45 || code > 127) ? code
-			: code < 46 ? 65               // -
-			: code < 48 ? code - 1
-			: code < 58 ? code + 18        // 0-9
-			: code < 65 ? code - 11
-			: code < 91 ? code + 11        // A-Z
-			: code < 97 ? code - 37
-			: code < 123 ? code + 5        // a-z
-			: code - 63
-	}
+	if ((a+="") == (b+="")) return 0;
 
-
-	if ((a+="") != (b+="")) for (;codeB;) {
+	for (;codeB;) {
 		codeA = getCode(a, posA++)
 		codeB = getCode(b, posB++)
 
 		if (codeA < 76 && codeB < 76 && codeA > 66 && codeB > 66) {
 			codeA = getCode(a, posA, posA)
-			codeB = getCode(b, posB, posA = i)
-			posB = i
+			codeB = getCode(b, posB, posB)
 		}
 
 		if (codeA != codeB) return (codeA < codeB) ? -1 : 1
 	}
-	return 0
 }
 
 try {
